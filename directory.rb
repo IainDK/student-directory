bc36a1fa55e0b85da4f41191f8b15142e18f8268
iaink <iainkinnish@googlemail.com>
2016-09-15 21:17:58 +0000

    Added comments to make the code easier to understand.

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# Firstly, we print the list of students using the puts method.
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,10 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+=begin We then print the total number of students we have. 
+       Here we use the 'print' method which allows us to divide the code between lines, because...
+       unlike 'puts', print does not wrap the line.
+=end
 print "Overall, we have "
 print 11
 print " great students."
