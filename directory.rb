0ff0f3f4372dbd6b175fbe74984b3c96480e510c
iaink <iainkinnish@googlemail.com>
2016-09-17 09:42:19 +0000

    Added .each_with_index method.

--- directory.rb
+++ directory.rb
@@ -21,8 +21,8 @@
     end
     
     def print_class(students)
-      students.each do |student|                                  
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      students.each_with_index do |student, index|                                  
+        puts "#{index + 1}: " "#{student[:name]} (#{student[:cohort]} cohort)"
       end
     end
     
