cc9edd15236d90d7611b15c7c0bbf9a2396596d2
iaink <iainkinnish@googlemail.com>
2016-09-18 11:39:34 +0000

    Added a .length method & cleaned the code up some more.

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
     def input_students
-        puts "Please enter the name(s) of the students:\n To finish, just hit return twice.\n\n" # Double line break to make the program look neater.
+        puts "Please enter the names of the students:\n To finish, just hit return twice.\n\n" 
 
         students = []
 
@@ -16,7 +16,7 @@
     end
 
     def print_header
-        puts "The students of Villains Academy\n\n"                             # As above, the addition of a double line break helps the program look more presentable.
+        puts "The students of Villains Academy\n\n"    
     end
     
     def print_class(students)
@@ -25,15 +25,23 @@
       end
     end
 
-    def print_begins_with(students)                                             # Method for filtering students whose name begins with a letter of the user's choice.
-  puts "\n"                                                                     # Line break for presentation purposes.
-  print "Please input a letter to filter students by: "                         # Tell the user what we want from them.
-  letter = gets.chomp.capitalize                                                # Get the user's input and capitalize it so to match the names that were previously entered.
-  puts "\n"                                                                     # Line break for presentation purposes.
-  select_students = students.select { |student| student[:name][0] == letter }   # .select method used to filter for students whose first [0] name begins with...
-  select_students.each { |student| puts student[:name] }                        # Do this for .each student, and puts the result.
+    def print_begins_with(students)
+  puts "\n"
+  print "Please input a letter to filter students by: "
+  letter = gets.chomp.capitalize
+  puts "\nThank you. Here are all matches found within the directory:\n\n"
+  select_students = students.select { |student| student[:name][0] == letter }
+  select_students.each { |student| puts student[:name] }
     end
-    
+
+    def print_if_length(students)
+        puts "\n"
+        puts "And, here is a list of all students whose names contain 12 characters or less:"   # This program works similarly to the one above
+        puts "\n"
+        select_students = students.select { |student| student[:name].length <= 13 }   # We use the number 13 to compensate for the space between the first and last name.
+        select_students.each { |student| puts student[:name] }
+    end
+
     def print_footer(names)
         puts "\nOverall, we have #{names.count} great students."
     end
@@ -42,4 +50,5 @@ students = input_students
 print_header
 print_class(students)
 print_footer(students)
-print_begins_with(students)                                                     # Calling our newly defined method.
+print_begins_with(students)
+print_if_length(students)         # Printing the new method.
